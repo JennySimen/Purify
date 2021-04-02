3ef5d736e5323bc550b2e17c85ccf4b6c4ef3c70
JennySimen <yakamjennifer@gmail.com>
2021-04-03 00:07:28 +0100

    Adjusted the home activity and created the filter activvity

--- app/src/main/java/com/example/purify/HomeActivity.java
+++ app/src/main/java/com/example/purify/HomeActivity.java
@@ -1,14 +1,19 @@
 package com.example.purify;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private CardView cardViewFilter;
+    private CardView cardViewIrrigation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
     }
 }
