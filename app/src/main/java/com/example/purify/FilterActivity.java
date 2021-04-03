109ffba25685bc4c9b7b4828010d652f88ec39b3
JennySimen <yakamjennifer@gmail.com>
2021-04-03 18:43:27 +0100

    Adjusted the FilterActivity xml

--- app/src/main/java/com/example/purify/FilterActivity.java
+++ app/src/main/java/com/example/purify/FilterActivity.java
@@ -9,12 +9,19 @@ import android.view.View;
 
 public class FilterActivity extends AppCompatActivity {
 
-
+    private CardView cardViewWell, cardViewStream, cardViewTank;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
 
-
+        cardViewWell = findViewById(R.id.well);
+        cardViewWell.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            Intent wellIntent = new Intent(FilterActivity.this, WellFilterActivity.class);
+            startActivity(wellIntent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/purify/WellFilterActivity.java
@@ -0,0 +1,14 @@
+package com.example.purify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WellFilterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_well_filter);
+    }
+}
