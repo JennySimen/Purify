5b888a4ecd1e66098db79cdfaec8039cd8bb8ab6
JennySimen <yakamjennifer@gmail.com>
2021-04-13 23:17:10 +0100

    Removed the expandable cardview

--- app/src/main/java/com/example/purify/WellFilterActivity.java
+++ app/src/main/java/com/example/purify/WellFilterActivity.java
@@ -12,47 +12,10 @@ import android.widget.LinearLayout;
 
 public class WellFilterActivity extends AppCompatActivity {
 
-    ImageButton arrow1;
-    LinearLayout hiddenView1;
-    CardView cardView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_well_filter);
 
-        cardView = findViewById(R.id.bleach);
-        arrow1 = findViewById(R.id.arrow_button1);
-        hiddenView1 = findViewById(R.id.hidden_view1);
-
-        arrow1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                // If the CardView is already expanded, set its visibility
-                //  to gone and change the expand less icon to expand more.
-                if (hiddenView1.getVisibility() == View.VISIBLE) {
-
-                    // The transition of the hiddenView is carried out
-                    //  by the TransitionManager class.
-                    // Here we use an object of the AutoTransition
-                    // Class to create a default transition.
-                    TransitionManager.beginDelayedTransition(cardView,
-                            new AutoTransition());
-                    hiddenView1.setVisibility(View.GONE);
-                    arrow1.setImageResource(R.drawable.ic_baseline_expand_more_24);
-                }
-
-                // If the CardView is not expanded, set its visibility
-                // to visible and change the expand more icon to expand less.
-                else {
-
-                    TransitionManager.beginDelayedTransition(cardView,
-                            new AutoTransition());
-                    hiddenView1.setVisibility(View.VISIBLE);
-                    arrow1.setImageResource(R.drawable.ic_baseline_expand_less_24);
-                }
-            }
-        });
     }
 }
